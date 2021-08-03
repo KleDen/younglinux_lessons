56c0263d96e75915cbd4bf07dad046ec99a65137
KleDen <asddsasdasddsasdasddsasd@gmail.com>
2021-08-03 23:28:53 +0300

    Сommit before mergin methods

--- Warriors_exercise/definitions.py
+++ Warriors_exercise/definitions.py
@@ -7,25 +7,21 @@ class Warrior:
         self.name = name
 
     def is_alive(self):
-        if self.hp > 0:
-            return True
-        else:
-            return False
+        return self.hp > 0
 
-    def det_damaged(self, damage):
+    def get_damaged(self, damage: int):
         self.hp -= damage
         print(self.name, "take", damage, "damage, he's now at", self.hp, "hp")
 
 
 def rand_hit(unit1: Warrior, unit2: Warrior):
     """Takes 2 units as a parameters and they randomly hit each other"""
+    units = [unit1, unit2]
+    random.shuffle(units)
+    attack, defend = units[0], units[1]
 
-    if unit1 == random.choice([unit1, unit2]):
-        unit2.hp -= 20
-        print(unit1.name, "was luckier and hit", unit2.name, "by 20! he's now at", unit2.hp, "hp")
-    else:
-        unit1.hp -= 20
-        print(unit2.name, "was luckier and hit", unit1.name, "by 20! he's now at", unit1.hp, "hp")
+    print(attack.name, "hit", defend.name, )
+    defend.get_damaged(20)
 
 
 def battle(unit1: Warrior, unit2: Warrior):
@@ -34,8 +30,9 @@ def battle(unit1: Warrior, unit2: Warrior):
         rand_hit(unit1, unit2)
 
         if not unit1.is_alive():
-            print(unit2.name, "Win this fight!!!")
-            break
+            winner = unit2
         if not unit2.is_alive():
-            print(unit1.name, "Win this fight!!!")
+            winner = unit1
+
+            print(winner.name, "Win this fight!!!")
             break
