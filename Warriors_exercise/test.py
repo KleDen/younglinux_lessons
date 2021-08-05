3b2d0a2719466909f93ff6274e5afd82ba0ee934
KleDen <asddsasdasddsasdasddsasd@gmail.com>
2021-08-05 03:42:35 +0300

    Updated tests to Warrior_exercise

--- Warriors_exercise/test.py
+++ Warriors_exercise/test.py
@@ -12,32 +12,36 @@ class TestWarrior(unittest.TestCase):
 
     def test_get_damaged(self):
         self.bob = Warrior("Bob")
-        self.bob.get_damaged(9)
-        self.assertEqual(self.bob.hp, 91)
+        self.bob.get_damaged(10)
+        self.assertEqual(self.bob.hp, 90)
         self.bob.get_damaged(90)
-        self.assertEqual(self.bob.hp, 1)
+        self.assertEqual(self.bob.hp, 0)
 
     def test_negative_damage(self):
         self.bob = Warrior("Bob")
-        self.assertRaises(ValueError, self.bob.get_damaged, -5)
-        self.assertRaises(ValueError, self.bob.get_damaged, -111)
+        self.assertRaises(ValueError, self.bob.get_damaged, -1)
+        self.assertRaises(ValueError, self.bob.get_damaged, -11111)
 
     def test_negative_hp(self):
         with self.assertRaises(ValueError):
             bob = Warrior("name", -9)
             bob = Warrior("name", 0)
 
+    # you can not set hp < 0
 
     def test_is_dead(self):
         self.bob = Warrior("Bob", 100)
-        self.assertTrue(self.bob.is_alive)
         self.mike = Warrior("Mike", 10)
 
-        self.mike.get_damaged(20)
+        self.assertTrue(self.bob.is_alive)
+        self.assertTrue(self.mike.is_alive)
+        # Now they are alive
         self.bob.get_damaged(100)
-        self.assertFalse(self.mike.is_alive())
-        self.assertFalse(self.bob.is_alive())
-
+        self.mike.get_damaged(20)
+        # Hit
+        self.assertFalse(self.bob.is_alive())  # hp = 0
+        self.assertFalse(self.mike.is_alive())  # hp = -10
+        # Now they Dead
 
 if __name__ == '__main__':
     unittest.main()
