78840a356a1248b98a9cd6fedd193ced9661cbea
KleDen <asddsasdasddsasdasddsasd@gmail.com>
2021-08-07 06:40:43 +0300

    Updated Encapsulation tests

--- Encapsulation/test.py
+++ Encapsulation/test.py
@@ -1,9 +1,15 @@
 import unittest
+from definitions import Triangle
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestHidenAttr(unittest.TestCase):
+    def test_private_attribute(self):
+        test_triangle = Triangle(1, 10)
+        with self.assertRaises(AttributeError):
+            test_triangle.width
+        with self.assertRaises(AttributeError):
+            test_triangle.height
+
 
 
 if __name__ == '__main__':
